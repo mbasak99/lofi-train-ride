dab05c35f307ef308799edf213b5dc738b1e43ad
Monark Basak <monarkb01@gmail.com>
2024-03-25 00:37:29 -0400

    Added range inputs for the audio streams.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,35 @@ import lofiTrain from "./gif/lofi_train_girl.gif";
 
 export default function Home() {
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center gap-8 p-24">
+    <div className="flex flex-col items-center">
+      <main className="flex min-h-screen flex-col items-center gap-8 p-10">
         <div>
-          <h1>Lofi Train Ride</h1>
+          <h1 className="text-4xl font-bold">Lofi Train Ride</h1>
         </div>
         <Image
           src={lofiTrain}
           width={400}
           height={300}
           className="rounded-lg"
-          alt="Lofi Train Ride GIF"
+          alt="Lofi Train Ride Animation"
           unoptimized={true}
         />
+        <div className="flex w-full p-10 flex-col gap-10 input-container">
+          <span className="flex flex-col gap-4">
+            <label htmlFor="music">Music: </label>
+            <input name="music" type="range" />
+          </span>
+          <span className="flex flex-col gap-4">
+            <label htmlFor="train-noise">Train Ambience: </label>
+            <input name="train-noise" type="range" />
+          </span>
+          <span className="flex flex-col gap-4">
+            <label htmlFor="people-talking">Nearby Conversations: </label>
+            <input name="people-talking" type="range" />
+          </span>
+        </div>
       </main>
       <footer className="m-10">Made by Monark</footer>
-    </>
+    </div>
   );
 }
